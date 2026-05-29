465f2f30dd0d6ed58eda6acfb579cd43a2dc4a3f
PavelDats1 <tiger206135@gmail.com>
2026-05-29 20:28:09 +0300

    Remove unnecessary files

--- banking/src/Transaction.cpp
+++
@@ -1,9 +0,0 @@
-#include "banking/Transaction.h"
-
-bool Transaction::transfer(Account& from, Account& to, double amount) {
-    if (from.withdraw(amount)) {
-        to.deposit(amount);
-        return true;
-    }
-    return false;
-}
